afc3068aa103450e63b7ad2cf08df233b47aabec
JimWest <j1m_w3st@web.de>
2016-11-27 18:37:47 +0100

    initial commit, working Unity3D Project with OpenCv inlcuded, can read the webcam picture and show it

---
+++ Assets/MagicMirror.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+using System.IO;
+
+public class MagicMirror : MonoBehaviour
+{
+
+    public GameObject background;
+
+    VideoCapture capture;
+    Mat webcamMat;
+    Material backgroundMaterial;
+    
+
+    // Use this for initialization
+    void Start()
+    {        
+        backgroundMaterial = background.GetComponent<Renderer>().material;
+
+        webcamMat = new Mat();
+        capture = new VideoCapture();
+
+        capture.Open(0);
+        if (!capture.IsOpened())
+        {
+            Debug.LogError("Unable to open the Camera");
+            this.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        using (Mat tempMat = capture.RetrieveMat())
+        {
+            // mirror image on y axis that its like a mirror
+            Cv2.Flip(tempMat, webcamMat, FlipMode.Y);
+            //Cv2.ImShow("Test", webcamMat);
+
+            Texture2D tex = OpenCvUtilities.MatToTexture2D(webcamMat);
+            
+            //File.WriteAllBytes(Application.dataPath + "/../SavedScreen.png", tex.EncodeToPNG());
+
+            backgroundMaterial.mainTexture = tex;
+        }
+
+    }
+
+    void OnDestroy()
+    {
+        webcamMat.Dispose();
+        capture.Release();
+        capture.Dispose();
+    }
+
+}
---
+++ Assets/OpenCvUtilities.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using OpenCvSharp;
+using System;
+
+public static class OpenCvUtilities
+{
+
+    public static Texture2D MatToTexture2D(Mat mat)
+    {
+        Texture2D tex = new Texture2D(mat.Width, mat.Height, TextureFormat.RGB24, false);
+
+        // needs to be flipped at x axis                        
+        using (Mat tempMat = mat.Flip(FlipMode.X))
+        {
+            // also wrong color (BGR instead of RGB), write it back to rgbMat
+            Cv2.CvtColor(tempMat, tempMat, ColorConversionCodes.BGR2RGB);
+
+            int size = (int)tempMat.Total() * tempMat.ElemSize();
+            tex.LoadRawTextureData(tempMat.Data, size);
+            tex.Apply();
+        }
+
+        return tex;
+    }
+
+    public static Mat MatToTexture2D(Texture2D tex)
+    {
+        Mat mat = new Mat(tex.height, tex.width, MatType.CV_8UC3, tex.GetRawTextureData());
+        // needs to be flipped at x axis                        
+        using (Mat tempMat = mat.Flip(FlipMode.X))
+        {
+            // also wrong color (BGR instead of RGB), write it back to rgbMat
+            Cv2.CvtColor(tempMat, tempMat, ColorConversionCodes.RGB2BGR);
+        }
+
+        return mat;
+    }
+
+}
